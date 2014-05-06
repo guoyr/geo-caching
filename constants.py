814ad8980b123f17bce2a8dd77750f6690c4cd15
Robert Guo <robertguo@me.com>
2014-05-06 19:54:09 -0400

    add user data to measurement

---
+++ constants.py
@@ -0,0 +1,9 @@
+IMAGE_UID_KEY = "image_uid_key"
+USER_UID_KEY = "user_id"
+LATENCY_DICT_KEY = "latency_dict_key"
+
+MASTER_SERVER_ID = "master_server_id"
+
+WEST_STORE_ID = "WEST"
+EAST_STORE_ID = "EAST"
+COORDINATOR_ID = "COORDINATOR"
