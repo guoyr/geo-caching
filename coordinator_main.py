9af00b4b1fd5a425e9b778ed69a516be2c81b074
Robert Guo <robertguo@me.com>
2014-05-05 23:51:45 -0400

    add autobahn socket for latency measurement information

--- coordinator_main.py
+++ coordinator_main.py
@@ -3,17 +3,27 @@ import sys
 
 from twisted.internet import selectreactor
 selectreactor.install
+
 from twisted.python import log
 from twisted.internet.endpoints import TCP4ServerEndpoint
-from measurement_handler import get_measurement_factory
+
+from autobahn.twisted.websocket import WebSocketServerFactory
+
+from measurement_handler import LatencyMeasurementProtocol
+
+
 def main():
 
     from twisted.internet import reactor    
 
     http_measurement_port = 8888
 
+
+    measurement_factory = WebSocketServerFactory()
+    measurement_factory.protocol = LatencyMeasurementProtocol
+
     measurement_endpoint =  TCP4ServerEndpoint(reactor, http_measurement_port)
-    measurement_endpoint.listen(get_measurement_factory())
+    measurement_endpoint.listen(measurement_factory)    
 
     log.startLogging(sys.stdout)
     reactor.run()
--- measurement_handler.py
+++ measurement_handler.py
@@ -1,7 +1,36 @@
 #handles image transfers
 from twisted.web.resource import Resource
 from twisted.web.server import Site
+from twisted.internet.task import LoopingCall
+
+from autobahn.twisted.websocket import WebSocketServerProtocol
+
 import json
+
+class LatencyMeasurementProtocol(WebSocketServerProtocol):
+
+    def onConnect(self, request):
+        print "Client connecting: {0}".format(request.peer)
+        lc = LoopingCall(self.sendLatencyInfo,None)
+        lc.start(1)
+
+    def onOpen(self):
+        print "WebSocket connection open."
+
+    def onMessage(self, payload, isBinary):
+        if isBinary:
+            print "Binary message received: {0} bytes".format(len(payload))
+        else:
+            print "Text message received: {0}".format(payload.decode('utf8'))
+        self.sendMessage(json.dumps(payload), isBinary)
+
+    def sendLatencyInfo(self, latency_dict):
+        print "send latency info"
+        latency_dict = {"east_latency":"1.004", "west_latency":"2.056"}
+        self.sendMessage(json.dumps(latency_dict), False)
+
+    def onClose(self, wasClean, code, reason):
+        print "WebSocket connection closed: {0}".format(reason)
 
 class MeasurementResource(Resource):
     isLeaf = True
