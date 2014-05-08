9502ae7e5a6e40fb45ed168aa9dd3be6c00f1b4d
jiachenz <jiachenz@usc.edu>
2014-05-08 19:18:51 -0400

    updated print statement for debuging to see if the correct update is applied to the db.

--- image_transfer_handler.py
+++ image_transfer_handler.py
@@ -145,7 +145,7 @@ def get_image(name,user):
     time = datetime.datetime.now()
     image_info["views"] = image_info["view"] + 1
     image_info["last_used_time"] = time
-
+    print ("picture: " + uid + " viewed " + image_info["views"] + " times. Last used time: " + image_info["last_used_time"])
     return f
 
 
