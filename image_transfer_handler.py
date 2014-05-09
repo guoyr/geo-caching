554a6de39af681e0239b6dd7c814bdbeefdeda43
jiachenz <jiachenz@usc.edu>
2014-05-08 21:06:37 -0400

    fixed the error of the wrong argument of callRemote of add_access_record() in render_GET()

--- image_transfer_handler.py
+++ image_transfer_handler.py
@@ -40,7 +40,7 @@ class ImageTransferResource(Resource):
 
             #add access record first
             def add_access_record(protocol):
-                return protocol.callRemote(AddAccessRecord, USER_UID_KEY=user, PREFERRED_STORE_KEY=SERVER_ID, IS_SAVE_ACTION=False)
+                return protocol.callRemote(AddAccessRecord, user_id=user, PREFERRED_STORE_KEY=SERVER_ID, IS_SAVE_ACTION=False)
             d.addCallback(add_access_record)
             sys.stdout.flush()
             if image:
