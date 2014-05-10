e57f86014b8331f2be5a5faa4ff0fe8e5f71915f
Robert Guo <robertguo@me.com>
2014-05-10 17:05:04 -0400

    bug fix

--- measurement_handler.py
+++ measurement_handler.py
@@ -40,7 +40,7 @@ class LatencyMeasurementProtocol(WebSocketServerProtocol):
             callTime = 0.0
             print "send latency for user: "  + userID
             for from_key, to_key, latency in LatencyCache[userID]:
-                print "animation from " + from_key + "to " + to_key
+                print "animation from " + from_key + "to " + to_key + "latency " + latency
                 info = {}
                 x, y = self._getUserCoords(from_key, to_key)
                 #use tokey to determine from key
