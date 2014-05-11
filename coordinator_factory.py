3a38b2055b85ac0f059dc72734f25fcf66479af2
Robert Guo <robertguo@me.com>
2014-05-11 15:36:22 -0400

    bug fix

--- coordinator_factory.py
+++ coordinator_factory.py
@@ -87,7 +87,7 @@ class CoordinatorProtocol(AMP):
         d = FactoryManager().get_store_client_deferred(serverIDToServer(new_master))
 
         def notify_new_master(protocol):
-            return protocol.callRemote(InitiateMasterChange, user_uid_key=user, old_master_key=old_master)
+            return protocol.callRemote(InitiateMasterChange, user_uid_key=str(user), old_master_key=str(old_master))
 
         d.addCallback(notify_new_master)
         #TODO: add errback, if cache not available, revert master change
