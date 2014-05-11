d16391313cc992b7f6f8de9ad21e84eef40e5cc8
Robert Guo <robertguo@me.com>
2014-05-11 16:15:40 -0400

    add print statements

--- measurement_handler.py
+++ measurement_handler.py
@@ -38,7 +38,7 @@ class LatencyMeasurementProtocol(WebSocketServerProtocol):
         for userID in LatencyCache.keys():
             callTime = 0.1
             print "send latency for user: "  + userID
-            for from_key, to_key, latency in LatencyCache[userID]:
+            for from_key, to_key, latency, img_name in LatencyCache[userID]:
                 print "animation from " + from_key + "to " + to_key + "latency " + str(latency)
                 info = {}
                 x, y = self._getUserCoords(from_key, to_key)
@@ -49,6 +49,7 @@ class LatencyMeasurementProtocol(WebSocketServerProtocol):
                 info["user_x"] = str(x)
                 info["user_y"] = str(y)
                 info["user_id"] = userID
+                info["img_name"] = str(img_name)[:8]
                 reactor.callLater(callTime, self.sendM, json.dumps(info), False)
                 callTime += latency/10
         LatencyCache.clear()
